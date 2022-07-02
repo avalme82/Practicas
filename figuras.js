aab32adc20f081bfbe7d3eeb13174a4a58615a49
Francisco Adrián Valdez Mejía <adrianvaldezme@hotmail.com>
2022-07-02 16:07:17 -0500

    terminadas formulas de cuadrado, circulo y triangulos equilatero e isoseles, pendiente triangulo escaleno

--- figuras.js
+++ figuras.js
@@ -1,55 +1,3 @@
-/*
-//código del cuadrado
-console.group("cuadrados");
-const ladoCuadrado = 4;
-console.log("Los lados del cuadrado miden " + ladoCuadrado);
-
-console.log("El perimetro del cuadrado mide " + perimetroCuadrado(4));
-
-// ladoCuadrado * ladoCuadrado;
-console.log("El área del cuadrado mide " + areaCuadrado(ladoCuadrado));
-
-console.groupEnd();
-
-//código del triangulo
-console.group("triangulo");
-const ladotriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5; 
-console.log("Los lados del cuadrado miden " 
-+ ladotriangulo1 + " cm, " 
-+ ladoTriangulo2 + " cm, " 
-+ baseTriangulo + " cm");
-console.log("La altura del triangulo mide " + alturaTriangulo);
-
-const perimetrotriangulo = ladotriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perimetro del triangulo mide " + perimetrotriangulo(ladotriangulo1, ladoTriangulo2, baseTriangulo));
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo)/2;
-console.log("El área del triangulo mide " + areaTriangulo(baseTriangulo, alturaTriangulo));
-
-
-console.groupEnd();
-
-
-//código del circulo
-console.group("circulo");
-const radioCirculo = 4;
-const PI = Math.PI;
-const diametroCirculo = radioCirculo * 2;
-const perimetroCirculo = diametroCirculo * PI;
-const diametro = diametroCirculo(radio);
-const areaCirculo = (radioCirculo*radioCirculo)*PI;
-console.log("El rádio del circulo mide " + radioCirculo + " cm");
-console.log("El PI mide " + PI);
-console.log("El diametro del circulo mide " + diametroCirculo(radioCirculo) + " cm");
-console.log("El perimetro del circulo mide " + perimetroCirculo(radioCirculo) + " cm");
-console.log("El área del circulo mide " + areaCirculo(radioCirculo) + " cm2");
-
-console.groupEnd();
-*/
-
 function perimetroCuadrado (lado) {
     return lado * 4;
 }
@@ -85,18 +33,18 @@ function calcularAreaCuadrado (){
 }
 function calcularPerimetroTriangulo (){
     const input1 = document.getElementById("ladoTriangulo1");
-    const value1 = parseInt(input1.value);
+    const value1 = parseFloat(input1.value);
     const input2 = document.getElementById("ladoTriangulo2");
-    const value2 = parseInt(input2.value);
+    const value2 = parseFloat(input2.value);
     const input3 = document.getElementById("baseTriangulo");
-    const value3 = parseInt(input3.value);
+    const value3 = parseFloat(input3.value);
     const perimetro = perimetrotriangulo(value1, value2, value3);
     alert(perimetro);
 }function calcularAreaTriangulo (){
     const input1 = document.getElementById("baseTriangulo");
-    const value1 = parseInt(input1.value);
+    const value1 = parseFloat(input1.value);
     const input2 = document.getElementById("alturaTriangulo");
-    const value2 = parseInt(input2.value);
+    const value2 = parseFloat(input2.value);
     const area = areaTriangulo(value1, value2);
     alert(area);
 }
@@ -112,43 +60,87 @@ function calcularAreaCirculo (){
     const area = areaCirculo(value);
     alert(area);
 }
-function calcularPitagoras (cateto1, hipotenusa){
+function calcularAlturaTriangulo (cateto1, hipotenusa){
 // la suma del cuadrado de los catetos es igual al cuadrado de la hipotenusa
 // hip¬2 = cat1¬2 + cat2¬2
 // hip-2 - cat1-2 = cat2-2
 // sqrt(hip-1 - cat1-2) = cat2
-    return Math.sqrt(Math.pow(cateto1,2)-Math.pow(hipotenusa,2))
+    let altura = Math.sqrt(Math.pow(cateto1,2)-Math.pow(hipotenusa,2));
+    console.log("Su altura es " + altura + " cm")
+    return altura;
+}
+function calcularHipotenusa (cateto1, cateto2){
+    let hipotenusa = Math.sqrt(Math.pow(cateto1,2)+Math.pow(cateto2,2));
+    console.log("Su hipotenusa es " + hipotenusa + " cm")
+    return hipotenusa;
+}
+function verificarlados (ladoA,ladoB,ladoC){
+    console.log("lado mayor "+ladoA+", lado medio "+ladoB+", lado menor "+ladoC)
+    if (ladoA <= ladoB + ladoC){
+        console.log("triangulo valido")
+    }
+}
+function verificarTrianguloEscaleno (lado1,lado2,lado3){
+    console.log("es un triangulo escaleno");
+    if (lado1 >= lado2 && lado2 >= lado3){
+        verificarlados(lado1,lado2,lado3);
+    }
+    else if (lado1 > lado3 && lado3 > lado2){
+        verificarlados(lado1, lado3, lado2);
+    }
+    else if (lado2 > lado1 && lado1 > lado3){   
+        verificarlados(lado2,lado1,lado3);
+    }
+    else if (lado2 > lado3 && lado3 > lado1){
+        verificarlados(lado2,lado3,lado1);
+    }
+    else if (lado3 > lado1 && lado1 > lado2){
+        verificarlados(lado3,lado1,lado2);
+    }
+    else if (lado3 > lado2 && lado2 > lado1){
+        verificarlados(lado3,lado2,lado1);
+    }
+
+/*
+    a > b > c
+    a > c > b
+    b > a > c
+    b > c > a
+    c > a > b
+    c > b > b
+*/   
+}
+function verificaTrianguloisoseles (cateto, base){
+    if (cateto+cateto > base){
+        let altura = calcularAlturaTriangulo(cateto, (base/2));
+        console.log("Es un triangulo Isoseles válido con altura de " + altura + " cm")
+    }
+    else
+        console.log("No es un triangulo válido")
+    
 }
 function verificarTipoTriangulo (){
     const input1 = document.getElementById("ladoTriangulo1");
-    const value1 = parseInt(input1.value);
+    const value1 = parseFloat(input1.value);
     const input2 = document.getElementById("ladoTriangulo2");
-    const value2 = parseInt(input2.value);
+    const value2 = parseFloat(input2.value);
     const input3 = document.getElementById("baseTriangulo");
-    const value3 = parseInt(input3.value);
-    let cat = 0; let hip =0;
+    const value3 = parseFloat(input3.value);
+    calcularHipotenusa(value1,value2);
     if (value1 == value2 && value1 == value3){
         console.log("Es un triangulo equilatero");
-        cat = value1; hip=value3;
+        altura = calcularAlturaTriangulo(value1, value3/2);
     }
-    else
-        if (value1 == value2){
-            console.log("lado1 " + value1+ " y lado2 " + value2 +" son iguales "+value3+" es distinto");
-            if (value1 + value2 < value3) {
-                console.log("No es un triangulo valido");
-            }
-            else
-                console.log("Es un triangulo valido");
+    else if (value1 != value2 && value2!= value3 && value1 != value3){
+            verificarTrianguloEscaleno(value1,value2,value3);
         }
-        else
-            if (value1 == value3){
-                console.log("lado1 y base son iguales");
+        else if (value1 == value2){
+                verificaTrianguloisoseles(value1,value3);
             }
-            else
-                if (value2 == value3){
-                    console.log("Lado2 y base son iguales");
-                }
-                else
-                    console.log("es un triangulo escaleno");
-    console.log("Su altura es " + calcularPitagoras(cat, (hip/2)));
+            else if (value1 == value3){
+                verificaTrianguloisoseles(value1, value2);
+            }
+            else if (value2 == value3){
+                verificaTrianguloisoseles(value2,value1);
+            }           
 }
