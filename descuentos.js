c7e7a57cc155845f0a34a4ccc14134fba409e1a1
Francisco Adrián Valdez Mejía <adrianvaldezme@hotmail.com>
2022-07-02 23:09:10 -0500

    finaliza taller porcentajes y descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,11 @@
 function calcularPrecioConDescuento(precio, descuento){
     return ((precio*(100 - descuento))/100);
 }
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = "El precio con descuento son: $"+calcularPrecioConDescuento(priceValue,discountValue);
+}
