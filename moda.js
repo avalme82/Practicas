cf00f113cdb2535cd4620a3314e3afff4f70bbaa
Francisco Adrián Valdez Mejía <adrianvaldezme@hotmail.com>
2022-07-03 23:12:43 -0500

    reorganizar carpeta

--- moda.js
+++
@@ -1,6 +0,0 @@
-function calcularModa (list1){
-    const count = {};
-    list1.map(function (item){ (count[item])?(count[item] += 1):(count[item] = 1)})
-    const listArray = Object.entries(count).sort(function (a,b){ return a[1] - b[1];})
-    return listArray[listArray.length - 1];
-}
--- promedio.js
+++
@@ -1,4 +0,0 @@
-function calcularMediaAritmetica (list){
-    const sumList = list.reduce( function (sum=0,newItem){ return sum+newItem; } );
-    return sumList/list.length;
-}
