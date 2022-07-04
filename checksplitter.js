c64faf93912f1186ba2aab05183649fd581b9d4c
Francisco Adrián Valdez Mejía <adrianvaldezme@hotmail.com>
2022-07-03 23:10:00 -0500

    ligera modificación probando reducir código

--- checksplitter.js
+++ checksplitter.js
@@ -1,13 +1,14 @@
+function formatPrecision (valor){ return parseFloat(valor.toFixed(2)); }
 function onClickCheckSplit(tip){
     const inputCheck = parseFloat(document.getElementById("check").value);
     const inputPeople = parseInt(document.getElementById("people").value);
     const tipByCheck = (inputCheck*tip)/100;
     const checkTipIncluded = document.getElementById("checkTipIncluded");
-    checkTipIncluded.innerText = inputCheck+tipByCheck;
+    checkTipIncluded.innerText = formatPrecision(inputCheck+tipByCheck);
     const checkTip = document.getElementById("checkTip");
-    checkTip.innerText = tipByCheck;
+    checkTip.innerText = formatPrecision(tipByCheck);
     const checkSplit = document.getElementById("checkSplit");
-    checkSplit.innerText = (inputCheck+tipByCheck)/inputPeople;
+    checkSplit.innerText = formatPrecision((inputCheck+tipByCheck)/inputPeople);
     const tipSplit = document.getElementById("tipSplit");
-    tipSplit.innerText = tipByCheck/inputPeople;
+    tipSplit.innerText = formatPrecision(tipByCheck/inputPeople);
 }
